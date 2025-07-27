7c11434da58c2a7553f9d982a925c610daa3a9e1
pudding483 <chesterccj483@gmail.com>
2025-07-27 17:31:54 +0800

    finish Day12-counter, Day8 , Day12-popUpForm and Day12-reactToDoList left

---
+++ Day_12/counter-app/src/CounterApp.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./CounterApp.css";
+import { AppContainer } from "./components/AppContainer.jsx";
+
+function CounterApp() {
+  return <AppContainer />;
+}
+
+export default CounterApp;
